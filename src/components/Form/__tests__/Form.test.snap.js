ae7c00f4e92e74e4d52f5f33c2489c1d3b98a3c0
simonxcode <simonxiong11@gmail.com>
2022-04-24 23:14:45 -0400

    write test cases

--- src/components/Form/__tests__/Form.test.snap.js
+++ src/components/Form/__tests__/Form.test.snap.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { shallow } from 'enzyme'
 import renderer from 'react-test-renderer'
 import Form from '../Form'
 
@@ -6,3 +7,14 @@ it('Form component should match snapshot', () => {
   const tree = renderer.create( < Form / > ).toJSON()
   expect(tree).toMatchSnapshot()
 });
+
+describe('Form component', () => {
+  it('should render component content', () => {
+    expect(shallow(<Form/>).find('form').length).toEqual(1)
+    expect(shallow(<Form/>).find('h1').length).toEqual(1)
+    expect(shallow(<Form/>).find('p').length).toEqual(2)
+    expect(shallow(<Form/>).find('label').length).toEqual(9)
+    expect(shallow(<Form/>).find('input').length).toEqual(9)
+    expect(shallow( < Form / > ).find('button').length).toEqual(1)
+  })
+})
--- src/components/Header/__tests__/Header.test.snap.js
+++ src/components/Header/__tests__/Header.test.snap.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { shallow } from 'enzyme'
 import renderer from 'react-test-renderer'
 import Header from '../Header'
 
@@ -6,3 +7,9 @@ it('Header component should match snapshot', () => {
   const tree = renderer.create( <Header />).toJSON()
   expect(tree).toMatchSnapshot()
 });
+
+describe('Header component', () => {
+  it('should render component content', () => {
+    expect(shallow(<Header/>).find('div').length).toEqual(1)
+  })
+})
