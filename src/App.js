4b52f14b80b9fa793373c0e6dff73d2ab8310383
simonxcode <simonxiong11@gmail.com>
2022-04-24 20:46:31 -0400

    create header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import Form from './components/Form';
+import Header from './components/Header'
+import Form from './components/Form'
 
 const App = () => {
   return (
-    <div className="bg-blue-100">
-      <p className="text-4xl text-center font-bold p-4">Fizz Buzz Fun</p>
+    <div>
+      <Header />
       <Form />
     </div>
   );
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => (
+  <div className="flex justify-center items-center text-4xl text-center font-semibold p-4 text-emerald-900 drop-shadow-xl">
+    Fizz Buzz Fun
+  </div>
+)
+
+export default Header
